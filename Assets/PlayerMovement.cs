957ea394081433017c50f84ea5b07c8c8656b621
ChilledLand <klavspurvins03@gmail.com>
2022-10-27 11:06:06 +0200

    Map

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+    
+{
+    public CharacterController2D controller;
+    public float runSpeed = 70f;
+    float HorizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+    // Move our character
+    void FixedUpdate()
+    {
+        controller.Move(HorizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false; 
+    }
+}
