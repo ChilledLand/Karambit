6f152797d5ad9f10c20802f2fe6bedeb01435f73
Mohamad Sh <mohamad.sh.eddin@gmail.com>
2022-11-24 11:13:56 +0100

    eddit bow

--- Assets/Scripts/bow.cs
+++ Assets/Scripts/bow.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class bow : MonoBehaviour
 {
+    public GameObject arrow;
+    public float arrowForce;
+    public Transform shotPoint;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 bowPos = transform.position;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = mousePos - bowPos;
+        transform.right = dir;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+
+        }
+
     }
+
+    void Shoot()
+    {
+        GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
+        newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * arrowForce;
+    }
+
 }
